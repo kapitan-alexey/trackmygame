763e7325e6ac5a9a6c6829871fce11dab5d366ff
kapitan-alexey <Shmateyevne@gmail.com>
2024-01-24 20:06:09 +0600

    changed game duration form

--- bgamemate/forms.py
+++ bgamemate/forms.py
@@ -1,13 +1,25 @@
-from django.forms import ModelForm, TextInput, NumberInput
+from django.forms import ModelForm, TextInput, NumberInput, ChoiceField, Select
 from django.utils.translation import gettext_lazy as _
 from .models import GameSession, Player
 
 class GameSessionForm(ModelForm):
 
+    DURATION_CHOICES = [
+    (15, '00:15'),
+    (30, '00:30'),
+    (45, '00:45'),
+    (60, '01:00'),
+    (90, '01:30'),
+    (120, '02:00'),
+    (150, '02:30'),
+    (180, '03:00'),
+    (300, '05:00'),
+    ]
+    turn_duration = ChoiceField(choices=DURATION_CHOICES, widget=Select(attrs={'class': 'duration-form create-game-form'}), initial='mm:ss')
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # self.fields['game'].label = "Игра"
-        self.fields['turn_duration'].label = _("Длительность хода (сек)")
+        self.fields['turn_duration'].label = _("Длительность хода")
 
     class Meta:
         model = GameSession
@@ -15,7 +27,7 @@ class GameSessionForm(ModelForm):
         # fields = ['game', 'turn_duration']
         widgets = {
             # 'game': Select(attrs={'class': 'create-game-form game-form'}),
-            'turn_duration': NumberInput(attrs={'class': 'create-game-form duration-form'}),
+            # 'turn_duration': NumberInput(attrs={'class': 'create-game-form duration-form'}),
         }
 
 
